a323d1e0b62a2b769532df2f47988018128c061d
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-11 18:09:14 +0200

    good night

--- src/testegithub3/testegithub3.java
+++ src/testegithub3/testegithub3.java
@@ -8,6 +8,8 @@ public class testegithub3 {
 		System.out.println("bom dia");
 		System.out.println("Boa tarde");
 		System.out.println("boa noite");
+		
+		System.out.println("good morning and good night");
 
 	}
 
